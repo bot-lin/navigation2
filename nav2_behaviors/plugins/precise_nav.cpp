9910b3524cd1043e99598d39f5d1c79311f2d5e0
bot-lin <zlin6364@hotmail.com>
2023-01-24 12:48:56 +0800

    precise nav

---
+++ nav2_behaviors/plugins/precise_nav.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <thread>
+#include <algorithm>
+#include <memory>
+#include <utility>
+
+#include "precise_nav.hpp"
+#include "tf2/utils.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "nav2_util/node_utils.hpp"
+
+using namespace std::chrono_literals;
+
+namespace nav2_behaviors
+{
+
+PreciseNav::PreciseNav()
+: TimedBehavior<PreciseNavAction>(),
+    feedback_(std::make_shared<PreciseNavAction::Feedback>()),
+    xv_vel_(0.0),
+    zw_vel_(0.0)
+{
+}
+
+PreciseNav::~PreciseNav() = default;
+
+void PreciseNav::onConfigure()
+{
+
+}
+
+Status PreciseNav::onRun(const std::shared_ptr<const PreciseNavAction::Goal> command)
+{
+
+}
+
+Status PreciseNav::onCycleUpdate()
+{
+
+}
+
+
+} // namespace name
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(nav2_behaviors::PreciseNav, nav2_core::Behavior)
+
---
+++ nav2_behaviors/plugins/precise_nav.hpp
@@ -0,0 +1,32 @@
+#ifndef NAV2_BEHAVIORS__PLUGINS__PRECISENAV_HPP_
+#define NAV2_BEHAVIORS__PLUGINS__PRECISENAV_HPP_
+
+#include <chrono>
+#include <string>
+#include <memory>
+
+#include "nav2_behaviors/timed_behavior.hpp"
+#include "nav2_msgs/action/precisenav.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+
+namespace nav2_behaviors
+{
+using PreciseNavAction = nav2_msgs::action::PreciseNav;
+
+class PreciseNav : public TimedBehavior<PreciseNavAction>
+{
+public:
+    PreciseNav();
+    ~PreciseNav();
+    Status onRun(const stf::shared_ptr<constPreciseNavAction::Goal> command) override;
+    void onConfigure() override;
+    Status onCycleUpdate() override;
+
+protected:
+    PreciseNavAction::Feedback::SharePtr feedback_;
+    double xv_vel_;
+    double zw_vel_;
+    
+};
+}
+#endif
