6bc3b18da4b20e2d4b348bd0ea96b61ae0ca09e5
bot-lin <zlin6364@hotmail.com>
2023-03-01 17:39:10 +0800

    Update precise_nav_action.cpp

--- nav2_behavior_tree/plugins/action/precise_nav_action.cpp
+++ nav2_behavior_tree/plugins/action/precise_nav_action.cpp
@@ -36,7 +36,7 @@ void PreciseNavAction::on_tick()
   }
   getInput("goal", goal_.pose);
 }
-void FollowPathAction::on_wait_for_result(
+void PreciseNavAction::on_wait_for_result(
   std::shared_ptr<const nav2_msgs::action::PreciseNav::Feedback>/*feedback*/)
 {
   getInput("goal", goal_.pose);
