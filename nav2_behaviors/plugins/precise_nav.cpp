c1572e920bd87244aef8eb3472e07a083b099fbc
bot-lin <zlin6364@hotmail.com>
2023-04-04 14:29:58 +0800

    Update precise_nav.cpp

--- nav2_behaviors/plugins/precise_nav.cpp
+++ nav2_behaviors/plugins/precise_nav.cpp
@@ -65,7 +65,8 @@ Status PreciseNav::onRun(const std::shared_ptr<const PreciseNavAction::Goal> com
     geometry_msgs::msg::PoseStamped pose_tmp;
     if (command->pose.header.frame_id != "odom")
     {
-        command->pose.header.stamp = node_.lock()->now();
+        auto node = std::make_shared<rclcpp::Node>("tmp_node");
+        command->pose.header.stamp = node->now();
         bool tf_response = nav2_util::transformPoseInTargetFrame(command->pose, pose_tmp,  *this->tf_, "odom", this->transform_tolerance_);
         if (!tf_response)
         {
@@ -97,7 +98,8 @@ Status PreciseNav::change_goal(const std::shared_ptr<const PreciseNavAction::Goa
     geometry_msgs::msg::PoseStamped pose_tmp;
     if (command->pose.header.frame_id != "odom")
     {
-        command->pose.header.stamp = node_.lock()->now();
+        auto node = std::make_shared<rclcpp::Node>("tmp_node");
+        command->pose.header.stamp = node->now();
         bool tf_response = nav2_util::transformPoseInTargetFrame(command->pose, pose_tmp,  *this->tf_, "odom", this->transform_tolerance_);
         if (!tf_response)
         {
