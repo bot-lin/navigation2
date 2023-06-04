51d286f30bd8c8459306e71c36a1f3cea2b51af4
bot-lin <zlin6364@hotmail.com>
2023-06-04 21:24:17 +0800

    Update move_odom.cpp

--- nav2_waypoint_follower/plugins/move_odom.cpp
+++ nav2_waypoint_follower/plugins/move_odom.cpp
@@ -70,10 +70,10 @@ bool MoveOdom::processAtWaypoint(
   if (!is_enabled_) {
     return true;
   }
-  RCLCPP_INFO(
-    logger_, "Arrived at %i'th waypoint, controlling fork height  to %i millimeter",
-    curr_waypoint_index,
-    height_control);
+  // RCLCPP_INFO(
+  //   logger_, "Arrived at %i'th waypoint, controlling fork height  to %i millimeter",
+  //   curr_waypoint_index,
+  //   height_control);
   if (!this->backup_client_ptr_->wait_for_action_server()) {
     RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
     rclcpp::shutdown();
