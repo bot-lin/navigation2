7b0ef1d6553e8c713787e50b3dc663f92a5f98c5
bot-lin <zlin6364@hotmail.com>
2023-06-25 17:41:13 +0800

    u

--- nav2_costmap_2d/include/nav2_costmap_2d/footprint_collision_checker.hpp
+++ nav2_costmap_2d/include/nav2_costmap_2d/footprint_collision_checker.hpp
@@ -83,8 +83,8 @@ public:
 
 protected:
   CostmapT costmap_;
-  double current_collision_x_;
-  double current_collision_y_;
+  unsigned int current_collision_x_;
+  unsigned int current_collision_y_;
 };
 
 
