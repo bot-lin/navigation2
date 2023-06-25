3bdd1dc333ab2baa1b4f958e5199f89a7a0a71f4
bot-lin <zlin6364@hotmail.com>
2023-06-25 13:57:09 +0800

    Update regulated_pure_pursuit_controller.cpp

--- nav2_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
+++ nav2_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
@@ -421,7 +421,7 @@ auto rotate_pose = getLookAheadPoint(curvature_lookahead_dist_, transformed_plan
     //turn left
     RCLCPP_INFO(logger_, "RegulatedPurePursuitController detected collision ahead!");
     geometry_msgs::msg::PoseStamped transformed_pose;
-    auto collision_point = collision_checker_.current_collision_point_;
+    auto collision_point = collision_checker_->getCurrentCollisionPoint();
     collision_pose_msg_.pose.position.x = collision_point.x;
     collision_pose_msg_.pose.position.y = collision_point.y;
 
