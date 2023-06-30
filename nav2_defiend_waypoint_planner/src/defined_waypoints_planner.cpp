33a5337bd321a43d742b9fe3505300ecea565b69
bot-lin <zlin6364@hotmail.com>
2023-06-30 14:22:57 +0800

    Update defined_waypoints_planner.cpp

--- nav2_defiend_waypoint_planner/src/defined_waypoints_planner.cpp
+++ nav2_defiend_waypoint_planner/src/defined_waypoints_planner.cpp
@@ -307,6 +307,9 @@ nav_msgs::msg::Path DefinedWaypoints::createPlan(
   global_path.header.frame_id = global_frame_;
   DoublePoint closestPoint = findClosestPoint({start.pose.position.x, start.pose.position.y}, poses_);
   RCLCPP_INFO(node_->get_logger(), "Closest point to start is: %f, %f", closestPoint.first, closestPoint.second);
+
+  origin_y_ = origin_y_ + resolution_ * height;
+
   unsigned int start_y_index = std::floor((closestPoint.second - origin_y_) / resolution_);
   unsigned int start_x_index = std::floor((closestPoint.first - origin_x_) / resolution_);
   closestPoint = findClosestPoint({goal.pose.position.x, goal.pose.position.y}, poses_);
