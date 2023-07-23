c156364d536a7c9483d80943815655ad9eb9458d
bot-lin <zlin6364@hotmail.com>
2023-07-23 14:41:04 +0800

    wait command added

---
+++ nav2_behaviors/plugins/wait_command.cpp
@@ -0,0 +1,94 @@
+// Copyright (c) 2022 Joshua Wallace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <utility>
+
+#include "wait_command.hpp"
+#include "nav2_util/node_utils.hpp"
+
+namespace nav2_behaviors
+{
+WaitCommand::WaitCommand()
+: TimedBehavior<WaitCommandAction>(),
+  feedback_(std::make_shared<WaitCommandAction::Feedback>())
+{}
+
+void WaitCommand::onConfigure()
+{
+  auto node = node_.lock();
+  if (!node) {
+    throw std::runtime_error{"Failed to lock node"};
+  }
+
+  preempt_wait_sub_ = node->create_subscription<std_msgs::msg::Empty>(
+    "wait_util_command", rclcpp::SystemDefaultsQoS(),
+    std::bind(
+      &WaitCommand::preemptTeleopCallback,
+      this, std::placeholders::_1));
+}
+
+Status WaitCommand::change_goal(const std::shared_ptr<const WaitCommandAction::Goal> command)
+{
+  preempt_wait_ = false;
+  command_time_allowance_ = command->time_allowance;
+  end_time_ = steady_clock_.now() + command_time_allowance_;
+  return Status::SUCCEEDED;
+}
+
+Status WaitCommand::onRun(const std::shared_ptr<const WaitCommandAction::Goal> command)
+{
+  preempt_wait_ = false;
+  command_time_allowance_ = command->time_allowance;
+  end_time_ = steady_clock_.now() + command_time_allowance_;
+  return Status::SUCCEEDED;
+}
+
+void WaitCommand::onActionCompletion()
+{
+  preempt_wait_ = false;
+}
+
+Status WaitCommand::onCycleUpdate()
+{
+  feedback_->current_teleop_duration = elasped_time_;
+  action_server_->publish_feedback(feedback_);
+
+  rclcpp::Duration time_remaining = end_time_ - steady_clock_.now();
+  if (time_remaining.seconds() < 0.0 && command_time_allowance_.seconds() > 0.0) {
+    stopRobot();
+    RCLCPP_WARN_STREAM(
+      logger_,
+      "Exceeded time allowance before reaching the " << behavior_name_.c_str() <<
+        "goal - Exiting " << behavior_name_.c_str());
+    return Status::SUCCEEDED;
+  }
+
+  // user states that teleop was successful
+  if (preempt_wait_) {
+    stopRobot();
+    return Status::SUCCEEDED;
+  }
+  return Status::RUNNING;
+}
+
+
+void WaitCommand::preemptTeleopCallback(const std_msgs::msg::Empty::SharedPtr)
+{
+  preempt_wait_ = true;
+}
+
+}  // namespace nav2_behaviors
+
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(nav2_behaviors::WaitCommand, nav2_core::Behavior)
---
+++ nav2_behaviors/plugins/wait_command.hpp
@@ -0,0 +1,82 @@
+// Copyright (c) 2022 Joshua Wallace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_BEHAVIORS__PLUGINS__WAIT_COMMAND_HPP_
+#define NAV2_BEHAVIORS__PLUGINS__WAIT_COMMAND_HPP_
+
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/empty.hpp"
+#include "nav2_behaviors/timed_behavior.hpp"
+#include "nav2_msgs/action/wait_command.hpp"
+
+namespace nav2_behaviors
+{
+using WaitCommandAction = nav2_msgs::action::WaitCommand;
+
+/**
+ * @class nav2_behaviors::WaitCommand
+ * @brief An action server behavior for wait command
+ */
+class WaitCommand : public TimedBehavior<WaitCommandAction>
+{
+public:
+  WaitCommand();
+
+  /**
+   * @brief Initialization to run behavior
+   * @param command Goal to execute
+   * @return Status of behavior
+   */
+  Status onRun(const std::shared_ptr<const WaitCommandAction::Goal> command) override;
+  Status change_goal(const std::shared_ptr<const WaitCommandAction::Goal> command) override;
+
+  /**
+   * @brief func to run at the completion of the action
+   */
+  void onActionCompletion() override;
+
+  /**
+   * @brief Loop function to run behavior
+   * @return Status of behavior
+   */
+  Status onCycleUpdate() override;
+
+protected:
+  /**
+   * @brief Configuration of behavior action
+   */
+  void onConfigure() override;
+
+  /**
+   * @brief Callback function to preempt wait command
+   * @param msg empty message
+   */
+  void preemptTeleopCallback(const std_msgs::msg::Empty::SharedPtr msg);
+
+  WaitCommandAction::Feedback::SharedPtr feedback_;
+  bool preempt_wait_{false};
+
+  // subscribers
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr preempt_wait_sub_;
+
+  rclcpp::Duration command_time_allowance_{0, 0};
+  rclcpp::Time end_time_;
+};
+}  // namespace nav2_behaviors
+
+#endif  // NAV2_BEHAVIORS__PLUGINS__WAIT_COMMAND_HPP_
