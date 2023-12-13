9b39036d9661bbcadff04f1cf85424dbebf81afc
bot-lin <zlin6364@hotmail.com>
2023-12-14 00:50:40 +0800

    Update roller_control_action.hpp

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/roller_control_action.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/roller_control_action.hpp
@@ -57,7 +57,7 @@ public:
         BT::InputPort<std::string>(
         "task_name",
         "task name: eg roller_control"),
-        BT::InputPort<std::vector<double>>("params", std::vector<double>(), "params"),
+        BT::InputPort<std::vector>("params", std::vector<double>(), "params"),
       });
   }
 
