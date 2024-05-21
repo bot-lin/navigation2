dfcdaba2fc9d29753ca42de2b43e661c2d07927f
bot-lin <zlin6364@hotmail.com>
2024-05-21 23:50:49 +0800

    Update precise_nav.cpp

--- nav2_behaviors/plugins/precise_nav.cpp
+++ nav2_behaviors/plugins/precise_nav.cpp
@@ -138,13 +138,14 @@ Status PreciseNav::onRun(const std::shared_ptr<const PreciseNavAction::Goal> com
                     pose_tmp.pose.orientation.y, 
                     pose_tmp.pose.orientation.z,
                     pose_tmp.pose.orientation.w);
-    bool tf_response = nav2_util::transformPoseInTargetFrame(pose_tmp, pose_tmp,  *this->tf_, this->robot_base_frame_, this->transform_tolerance_);
+    geometry_msgs::msg::PoseStamped pose_tmp1;
+    bool tf_response = nav2_util::transformPoseInTargetFrame(pose_tmp, pose_tmp1,  *this->tf_, this->robot_base_frame_, this->transform_tolerance_);
     if (!tf_response)
     {
         RCLCPP_ERROR(this->logger_, "Failed to transform goal pose in %s frame from %s", this->robot_base_frame_.c_str(), command->pose.header.frame_id.c_str());
         return Status::FAILED;
     }
-    target_x_in_robot_frame_ = pose_tmp.pose.position.x;
+    target_x_in_robot_frame_ = pose_tmp1.pose.position.x;
     RCLCPP_INFO(this->logger_, "target x in robot frame: %f", target_x_in_robot_frame_);
     tf2::Matrix3x3 m(q);
     double roll, pitch, yaw;
